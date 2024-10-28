7b907d93a0b97c78267ebb15e91905ca615428ed
mattisnowman <matthias@rauter.it>
2024-10-28 09:45:02 +0100

    remove rocky setup

--- cpp/setup_rocky.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo dnf install -y openmpi openmpi-devel
-
-export PATH=/usr/lib64/openmpi/bin:$PATH
-export LD_LIBRARY_PATH=/usr/lib64/openmpi/lib/:$LD_LIBRARY_PATH
-export PSM3_DEVICES='self,shm'
